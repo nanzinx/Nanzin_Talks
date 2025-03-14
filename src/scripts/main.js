b7f5680f096726b8cec8dba864daac5373385023
Renan Leite <renanleites182@gmail.com>
2025-03-14 17:50:58 -0300

    fazendo o contador automatizado e o build no json

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataDoEvento = new Date ("Oct 25, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciAteOEvento % diasEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
